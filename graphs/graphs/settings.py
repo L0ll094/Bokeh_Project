4976d5088357351709da9b6a92a6fa81ec1eb2de
Louise <louise.stjern@alfalaval.com>
2020-11-12 08:06:06 +0100

    Removed secret key

--- graphs/graphs/settings.py
+++ graphs/graphs/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ekz^i4wvgm#@bo@d!_$)oso72l=b4bu+pt012qsx$duon2p2cf'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
